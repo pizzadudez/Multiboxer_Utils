593f99eec479a20e30fff78c4cc8926fcbdfb217
pizzadudez <40966394+pizzadudez@users.noreply.github.com>
2019-07-12 00:50:17 +0300

    auto-looting utility

--- main.lua
+++ main.lua
@@ -11,15 +11,37 @@ local StdUi = LibStub('StdUi')
 -- Addon Initialization
 -- ============================================================================
 
-function Utils:OnInitialize()
-
-end
-
 function Utils:OnEnable()
+    -- Enable AutoLoot:
+    if GetCVar("autoLootDefault") == "0" then
+        SetCVar( "autoLootDefault", 1 )
+    end
 
+    self:RegisterEvent('LOOT_READY')
 end
 
 
 -- ============================================================================
 -- Event Handlers
 -- ============================================================================
+
+function Utils:LOOT_READY()
+    local lootCount = GetNumLootItems()
+    if lootCount == 0 then
+        CloseLoot()
+    end
+
+    for slot = lootCount, 1, -1 do
+        local isLocked = select(6, GetLootSlotInfo(slot))
+        if isLocked == false then
+            LootSlot(slot)
+        end
+    end
+    lootCount = GetNumLootItems()
+    
+    if lootCount > 0 then
+        self:ScheduleTimer('LOOT_READY', 0.5)
+    else
+        CloseLoot()
+    end
+end
